e7d8d63c74206ada9ded7947387e65a705873ba7
秦浩 <2807645688@qq.com>
2019-04-12 10:55:41 +0800

    fill some QA

---
+++ LeetCode/src/BaseballGame.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class BaseballGame {
+	public int calPoints(String[] ops) {
+		Stack<Integer> stack=new Stack();
+		for(String word:ops){
+			if ("C".equals(word)) {
+				stack.pop();
+			}
+			else if("D".equals(word)){
+				stack.push(stack.peek()*2);
+			}
+			else if("+".equals(word)){
+				int temp1=stack.pop();
+				Integer num=stack.peek()+temp1;
+				stack.push(temp1);
+				stack.push(num);
+			}
+			else{
+				stack.push(Integer.parseInt(word));
+				stack.push(Integer.parseInt(word));
+			}
+		}
+		int sum=0;
+		while (!stack.empty()){
+			sum+=stack.pop();
+		}
+		return sum;
+	}
+}
