82c8d00be769191e13290975574c20eb4a15ca9c
秦浩 <2807645688@qq.com>
2019-05-13 09:18:59 +0800

    fill some QA

---
+++ LeetCode/src/JewelsandStones.java
@@ -0,0 +1,24 @@
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class JewelsandStones {
+
+	public int numJewelsInStonesForFaster(String J, String S) {
+		int[] tempResult=new int[58];
+		for(char type :J.toCharArray()){
+			tempResult[type-'A']=1;
+		}
+		int count=0;
+		for(char stone :S.toCharArray()){
+			count+=tempResult[stone-'A'];
+		}
+		return count;
+	}
+
+	public int numJewelsInStones(String J, String S) {
+		List<Integer> type= J.chars().boxed().collect(Collectors.toList());
+		return (int)S.chars().filter(m->type.contains(m)).count();
+	}
+}
