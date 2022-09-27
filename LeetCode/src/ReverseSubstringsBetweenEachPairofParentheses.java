b90a6242daacfc63c55589dce36bc654bd0951a6
jusda-cd-d-qinhao <qinhao@jusda.int>
2022-09-27 16:29:01 +0800

    add ReverseSubstringsBetweenEachPairofParentheses

---
+++ LeetCode/src/ReverseSubstringsBetweenEachPairofParentheses.java
@@ -0,0 +1,49 @@
+/**
+ * You are given a string s that consists of lower case English letters and brackets.
+ * <p>
+ * Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+ * <p>
+ * Your result should not contain any brackets.
+ */
+public class ReverseSubstringsBetweenEachPairofParentheses {
+	public static void main(String[] args) {
+		ReverseSubstringsBetweenEachPairofParentheses parentheses = new ReverseSubstringsBetweenEachPairofParentheses();
+		System.out.println(parentheses.reverseParentheses("abcd"));
+	}
+
+	public String reverseParentheses(String s) {
+		String result = s;
+		String reverseResult = reverse(s);
+		if (reverseResult != null) {
+			result = reverseResult;
+		}
+		while (reverseResult != null) {
+			reverseResult = reverse(reverseResult);
+			if (reverseResult != null) {
+				result = reverseResult;
+			}
+		}
+		return result;
+	}
+
+	public String reverse(String s) {
+		StringBuilder stringBuilder = new StringBuilder();
+		int leftIndex = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
+				leftIndex = i;
+			}
+			if (s.charAt(i) == ')') {
+				String tempValue = s.substring(leftIndex + 1, i);
+				StringBuffer reverse = new StringBuffer();
+				reverse.append(s, 0, leftIndex);
+				reverse.append(new StringBuffer(tempValue).reverse());
+				reverse.append(s, i + 1, s.length());
+				return reverse.toString();
+			}
+		}
+		return null;
+	}
+
+
+}
